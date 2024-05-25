7efb0f47ce735107200de7c11534cf1654fa1689
Precious <adeogunprecious913@gmail.com>
2024-05-25 14:22:39 +0100

    Update NoProjectSelected.jsx

--- src/Components/NoProjectSelected.jsx
+++ src/Components/NoProjectSelected.jsx
@@ -1,5 +1,5 @@
 import noProjectImage from '../assets/no-projects.png'
-import Button from './button'
+import Button from './Button'
 
 export default function NoProjectSelected ({onStartAddProject}){
     return <div className="mt-24 text-center w-2/3">
